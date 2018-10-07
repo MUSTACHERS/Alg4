66fd848b8e140ee3607e4f7a1398ea25ccd9b49d
MUSTACHERS <chaste86@163.com>
2018-10-07 15:08:41 +0800

    Search

---
+++ src/Searching/text.java
@@ -0,0 +1,16 @@
+package Searching;
+
+public class text {
+	public static void main(String[] args) {
+		System.out.println(0 % 5);
+		System.out.println(1 % 5);
+		System.out.println(2 % 5);
+		System.out.println(3 % 5);
+		System.out.println(4 % 5);
+		System.out.println(5 % 5);
+		System.out.println(6 % 5);
+		System.out.println(7 % 5);
+		System.out.println(8 % 5);
+		System.out.println(9 % 5);
+	}
+}
